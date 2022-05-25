182633279c6a4ce0769b93dbe3aecd8567e086d4
Lucas Santos <lukas_santos.silva@hotmail.com>
2022-05-25 16:20:06 -0300

    to calculate your salary with discount (Brazilian taxes)

---
+++ salary.py
@@ -0,0 +1,24 @@
+htworked = int(input('Inform the number of hours worked: '))
+hextra = int(input('Enter the number of overtime hours: '))
+vhour = int(input('Write the value of the hour: '))
+hovertime = int
+sbruto = int
+inss = int
+fgts = int
+sliquid = int
+
+hovertime == 100
+
+def new_func(htworked, hextra, vhour, hovertime):
+    sbruto = htworked + hextra * (1 + (hovertime / 100)) * vhour
+    return sbruto
+
+sbruto = new_func(htworked, hextra, vhour, hovertime)
+inss = sbruto * 9/100
+fgts = sbruto * 8/100
+sliquid = sbruto - inss
+
+print('The gross salary is:', sbruto)
+print('The net salary is: ', sliquid)
+print('The INSS is', inss)
+print('The FGTS is', fgts)
